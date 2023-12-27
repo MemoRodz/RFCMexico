55dc7da5027a6e2ab8eed23f639dd32a9cb12f08
MemoRodz <gmo.rodriguez@gmail.com>
2023-12-27 13:55:33 -0600

    Actualizando contenido de directorio

--- CrearRFC/RFCTest/obj/Debug/net6.0/RFCTest.AssemblyInfo.cs
+++ CrearRFC/RFCTest/obj/Debug/net6.0/RFCTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RFCTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7280d8370a264f8677a0247c4524868c5cac7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RFCTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RFCTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
